4d7e6f42ad6a904759f8fde854ee2c2dabd65c03
kakas <shrimptrain@gmail.com>
2020-10-31 10:31:19 +0800

    Task 1: use could send email to hr@avancevl.com

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+
+  resources :pages, only: [] do
+    collection do
+      get 'home'
+      get 'mail'
+    end
+  end
 end
