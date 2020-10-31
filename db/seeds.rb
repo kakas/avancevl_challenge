4acf83021621bb43a8cd5eb26ff2da568a26f556
kakas <shrimptrain@gmail.com>
2020-10-31 10:41:44 +0800

    Task 2-1: import data to firebase

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "google/cloud/firestore"
+require "csv"
+
+def import_questions
+  firestore = Google::Cloud::Firestore.new(
+    project_id: 'avancevl-challenge',
+    credentials: Rails.root.join('firebase-adminsdk-secret.json'),
+  )
+
+  csv = CSV.read(Rails.root.join('db/seeds/questions.csv'), headers: true)
+
+  csv.each_slice(500) do |rows|
+    rows.each do |row|
+      # https://googleapis.dev/ruby/google-cloud-firestore/latest/index.html#batched-writes
+      firestore.batch do |b|
+        question_id = row['question_id']
+        b.set("questions/#{question_id}", row.to_h)
+      end
+    end
+  end
+end
+
+import_questions
