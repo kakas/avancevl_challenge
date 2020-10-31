30d306ff7919441545f14109734358a2f80790a4
kakas <shrimptrain@gmail.com>
2020-10-31 11:56:02 +0800

    add current_user helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  def current_user
+    @current_user ||= User.find_by(uid: session[:user_id])
+  end
+  helper_method :current_user
 end
